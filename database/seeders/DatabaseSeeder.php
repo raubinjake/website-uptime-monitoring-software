ebc0a776c2a84e3c169832d924e351f987752cf5
Robin Kumar <robinkumarmth74@gmail.com>
2026-05-12 11:39:04 +0530

    Seed multiple demo clients

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $client = Client::query()->firstOrCreate([
-            'email' => 'client@example.com',
-        ]);
+        $clients = [
+            'client@example.com' => [
+                'https://laravel.com',
+                'https://vuejs.org',
+            ],
+            'operations@example.com' => [
+                'https://github.com',
+                'https://www.php.net',
+                'https://www.mysql.com',
+            ],
+            'support@example.com' => [
+                'https://example.com',
+                'https://www.wikipedia.org',
+            ],
+            'engineering@example.com' => [
+                'https://developer.mozilla.org',
+                'https://www.npmjs.com',
+                'https://packagist.org',
+            ],
+        ];
 
-        foreach (['https://laravel.com', 'https://vuejs.org'] as $url) {
-            $client->websites()->firstOrCreate(['url' => $url]);
+        foreach ($clients as $email => $urls) {
+            $client = Client::query()->firstOrCreate(['email' => $email]);
+
+            foreach ($urls as $url) {
+                $client->websites()->firstOrCreate(['url' => $url]);
+            }
         }
     }
 }
